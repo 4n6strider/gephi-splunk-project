6b2e6ecd9b823650de192b4574147689c4ce61ce
Nicolas Stone <nstone@nstone-mbp15.sv.splunk.com>
2016-08-16 11:22:44 -0700

    small aesthetic changes to files

--- gephi_gss/app/torun.py
+++ gephi_gss/app/torun.py
@@ -3,8 +3,8 @@ import splunklib.results as results
 
 HOST = "localhost"
 PORT = 8089
-USERNAME = "admin"
-PASSWORD = "changeme"
+USERNAME = "CHANGEME" #CHANGEME
+PASSWORD = "CHANGEME" #CHANGME
 
 service = client.connect(
     host=HOST,
@@ -15,7 +15,7 @@ service = client.connect(
 kwargs_oneshot = {"earliest_time": "",
                   "latest_time": "now",
                   "output_mode": "csv"}
-searchquery_oneshot = 'search sourcetype=bank | dedup txtUsername | chart list(txtUsername) by src_ip | rename list(txtUsername) as user | eval count=mvcount(user) | search count>4'
+searchquery_oneshot = ''
 
 oneshotsearch_results = service.jobs.oneshot(searchquery_oneshot, **kwargs_oneshot)
 
--- script_server/app/templates/ipinf.py
+++ script_server/app/templates/ipinf.py
@@ -5,8 +5,8 @@ import splunklib.results as results
 
 HOST = "localhost"
 PORT = 8089
-USERNAME = "admin"
-PASSWORD = "nicstone"
+USERNAME = "CHANGEME" #CHANGEME
+PASSWORD = "CHANGEME" #CHANGEME
 
 service = client.connect(
     host=HOST,
@@ -17,7 +17,7 @@ service = client.connect(
 kwargs_oneshot = {"earliest_time": "",
                   "latest_time": "now",
                   "output_mode": "csv"}
-#searchquery_oneshot = 'search sourcetype=bank src_ip=' + sys.argv[1] + ' |  chart list(_time) list(txtUsername) list(field8) list(session_id) by src_ip | rename list(field8) as ua | fillnull value="Unknown" ua | mvexpand ua | eval ua=ua+"$" | mvcombine ua | mvexpand list(txtUsername) | dedup list(txtUsername) | rename list(txtUsername) as user | mvcombine user | eval count=mvcount(user) | eval notes=""'
+
 searchquery_oneshot = 'search sourcetype="sample" src_ip=' + sys.argv[1] + ' | table src_ip,timestamp,user,ua,session_id,count,notes'
 
 oneshotsearch_results = service.jobs.oneshot(searchquery_oneshot, **kwargs_oneshot)
